841d9ff54f3fec3c846bd6555497e9fe37419c84
DoumSof99 <sofiadmn99@gmail.com>
2023-11-21 17:55:19 +0200

    Solution

---
+++ CompressAndDecompressText/CompressAndDecompressText/Program.cs
@@ -0,0 +1,52 @@
+﻿using CompressAndDecompressText;
+using Microsoft.IO;
+using System.IO.Compression;
+using System.Text;
+
+public class Program 
+{
+    public static void Main() 
+    {
+        // Original Text
+        string originalText = "Hello, this is a simple example of compression and decompression text";
+
+        //Compress text
+        byte[] compressedData = CompressText(originalText);
+        Console.WriteLine("Compressed Data: " + Convert.ToBase64String(compressedData));
+
+        string decompressedText = DecompressText(compressedData);
+        Console.WriteLine("Decompressed Text: " + decompressedText);
+
+    }
+
+    private static byte[] CompressText(string originalText)
+    {
+        using (MemoryStream ms = MemoryManager.GetStream())
+        {
+            // Use GZipStream to compress the data
+            using (var gzipStream = new GZipStream(ms, CompressionMode.Compress, true))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(originalText);
+            }
+
+            // Return the compressed data as a byte array
+            return ms.ToArray();
+        }
+    }
+
+    private static string DecompressText(byte[] compressedData)
+    {
+        // Create a MemoryStream from the compressed data
+        using (var compressStream = MemoryManager.GetStream(compressedData))
+        {
+            // Use GZipStream to decompress the data
+            using (var gZipStream = new GZipStream(compressStream, CompressionMode.Decompress))
+            using (var reader = new StreamReader(gZipStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        
+    }
+}
---
+++ CompressAndDecompressText/CompressAndDecompressText/StreamMemoryManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressAndDecompressText
+{
+    // Use RecyclableMemoryStreamManager for efficient memory management
+    internal class StreamMemoryManager
+    {
+        private static RecyclableMemoryStreamManager? _instance;
+
+        internal static RecyclableMemoryStreamManager Instance
+        {
+            get
+            {
+                _instance = new RecyclableMemoryStreamManager();
+                return _instance;
+            }
+        }
+    }
+
+    public static class MemoryManager
+    {
+        public static MemoryStream GetStream()
+        {
+            // GetStream() creates a MemoryStream managed by the RecyclableMemoryStreamManager
+            return StreamMemoryManager.Instance.GetStream();   
+        }
+
+        public static MemoryStream GetStream(byte[] data)
+        {
+            // GetStream() creates a MemoryStream managed by the RecyclableMemoryStreamManager
+            return StreamMemoryManager.Instance.GetStream("data", data, 0, data.Length);
+        }
+    }
+}
